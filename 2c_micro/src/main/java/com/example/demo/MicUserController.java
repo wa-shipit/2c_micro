81fa5e58c57f4182e3eec78caf60fc027e5c1074
miya0916 <miyarui09166@gmail.com>
2023-11-24 14:48:35 +0900

    ユーザ登録アプリ

---
+++ 2c_micro/src/main/java/com/example/demo/MicUserController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MicUserController {
+	
+	//DBへつなぐとこ↓
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	//ページ表示用メソッド
+	@RequestMapping(path = "/micuser", method = RequestMethod.GET)
+	public String copGet() {
+		return "micuser";
+	}
+
+	//画面から何か入力をした時用
+	@RequestMapping(path = "/micuser", method = RequestMethod.POST)
+	public String copPost(String loginid, String password, Model model) {
+
+		//DBに繋ぐならこんな感じ(JdbcTemplate)
+		jdbcTemplate.update("INSERT INTO miclogin (loginid,password) VALUES(?,?);", loginid, password);
+
+//		model.addAttribute("loginidlist", loginid);
+//		model.addAttribute("passwordlist", password);
+
+		return "redirect:/micuser";
+	}
+}
