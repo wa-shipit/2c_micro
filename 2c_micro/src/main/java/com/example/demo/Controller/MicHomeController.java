62872c21660fa391f863b4cf2453ac16cbab326c
GOKKING <huidachuan314@gmail.com>
2023-11-28 12:17:57 +0900

    登録編集削除

--- 2c_micro/src/main/java/com/example/demo/Controller/MicHomeController.java
+++ 2c_micro/src/main/java/com/example/demo/Controller/MicHomeController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.Controller;
 
 import java.util.List;
 import java.util.Map;
---
+++ 2c_micro/src/main/java/com/example/demo/Controller/MicTodoController.java
@@ -0,0 +1,75 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MicTodoController {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@RequestMapping(path = "/mictodo", method = RequestMethod.GET)
+	public String copGet() {
+		
+		return "mictodo";
+	}
+	@RequestMapping(path = "/micadd", method = RequestMethod.GET)
+	public String copGet1() {
+		
+		return "micadd";
+	}
+	
+	@RequestMapping(path = "/micedit", method = RequestMethod.GET)
+	public String copGet2() {
+		
+		return "micedit";
+	}
+	
+	@RequestMapping(path = "/micdel", method = RequestMethod.GET)
+	public String copGet3() {
+		
+		return "micdel";
+	}
+	
+	@RequestMapping(path = "/micadd", method = RequestMethod.POST)
+	public String copPost(String month, String day, String todo,Model model) {
+		String user_id ="1111";
+		//DBに繋ぐならこんな感じ(JdbcTemplate)
+			jdbcTemplate.update("INSERT INTO todo valuse(?,?,?,?)",user_id,month,day,todo);
+
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
+		model.addAttribute("todo", todo);
+
+		return "micadd";
+	}
+	@RequestMapping(path = "/micedit", method = RequestMethod.POST)
+	public String cop(String month, String day, String todo,Model model) {
+
+		//DBに繋ぐならこんな感じ(JdbcTemplate)
+			jdbcTemplate.update("INSERT INTO todo valuse(?,?,?)",month,day,todo);
+
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
+		model.addAttribute("todo", todo);
+
+		return "micedit";
+	}
+	@RequestMapping(path = "/micdel", method = RequestMethod.POST)
+	public String c(String month, String day, String todo,Model model) {
+
+		//DBに繋ぐならこんな感じ(JdbcTemplate)
+			jdbcTemplate.update("INSERT INTO todo valuse(?,?,?)",month,day,todo);
+
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
+		model.addAttribute("todo", todo);
+
+		return "todo";
+	}
+}
