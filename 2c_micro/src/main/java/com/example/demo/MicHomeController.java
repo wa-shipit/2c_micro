267bb5eaeb560aa4cbf5e0bc75253125442b4fbb
kaichin67 <kaishin20031216@gmail.com>
2023-12-01 14:20:48 +0900

    予定表示

---
+++ 2c_micro/src/main/java/com/example/demo/MicHomeController.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MicHomeController {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@RequestMapping(path = "/michome", method = RequestMethod.GET)
+	public String copGet(Model model) {
+		
+		List<Map<String, Object>> resultList;
+		
+		resultList = jdbcTemplate.queryForList("select * from todo");
+		
+		model.addAttribute("selectResult", resultList);
+		
+		return "michome";
+	}
+	
+}
