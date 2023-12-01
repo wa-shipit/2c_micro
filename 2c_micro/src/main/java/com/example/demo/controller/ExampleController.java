126ded8128149b0b4bcf9592e9473001c30f5e1f
kuni29oft <kuni__>
2023-12-01 13:57:47 +0900

    追加更新削除完了

--- 2c_micro/src/main/java/com/example/demo/controller/ExampleController.java
+++ 2c_micro/src/main/java/com/example/demo/controller/ExampleController.java
@@ -1,23 +1,26 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-public class MicTodoController {
+@Controller
+public class ExampleController {
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
 	//コピペ用サンプル(ページ表示用メソッド)
-	@RequestMapping(path = "/micadd", method = RequestMethod.GET)
+	@RequestMapping(path = "/example", method = RequestMethod.GET)
 	public String copGet() {
 		return "example";
 	}
 
 	//コピペ用サンプル（画面から何か入力をした時用）
-	@RequestMapping(path = "/micadd", method = RequestMethod.POST)
+	@RequestMapping(path = "/example", method = RequestMethod.POST)
 	public String copPost(String example1, String example2, Model model) {
 
 		//DBに繋ぐならこんな感じ(JdbcTemplate)
@@ -28,5 +31,4 @@ public class MicTodoController {
 
 		return "example";
 	}
-
 }
